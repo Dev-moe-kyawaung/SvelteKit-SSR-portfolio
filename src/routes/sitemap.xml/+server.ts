3de0aab864418a77e30919c39c9cd0e8728b02b6
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-10 19:43:15 +0630

    Create +server.ts

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,24 @@
+import { site, projects } from '$utils/site';
+
+export const prerender = true;
+
+export function GET() {
+  const routes = ['/', '/about', '/projects', '/contact', ...projects.map((p) => `/projects/${p.slug}`)];
+  const urls = routes
+    .map(
+      (route) => `
+      <url>
+        <loc>${site.url}${route}</loc>
+      </url>`
+    )
+    .join('');
+
+  const body = `<?xml version="1.0" encoding="UTF-8" ?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls}
+  </urlset>`;
+
+  return new Response(body, {
+    headers: { 'Content-Type': 'application/xml' }
+  });
+}
